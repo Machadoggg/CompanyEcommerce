77eae2c2c81f19ea3d7c89881bfe8a9c8fb46b16
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-04-03 23:05:44 -0500

    Import project and libraries

--- Company.Ecommerce/CompanyEcommerce.Infrastructure.Data/ConnectionFactory.cs
+++ Company.Ecommerce/CompanyEcommerce.Infrastructure.Data/ConnectionFactory.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Company.Ecommerce.Transversal.Common;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace CompanyEcommerce.Infrastructure.Data
 {
