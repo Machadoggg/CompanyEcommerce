727f5a5fc74bafb910eb815fd8390c3da539f3e8
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-04-03 23:47:44 -0500

    Add operations in ICustomersRepository

--- Company.Ecommerce/CompanyEcommerce.Infrastructure.Interface/ICustomersRepository.cs
+++ Company.Ecommerce/CompanyEcommerce.Infrastructure.Interface/ICustomersRepository.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Company.Ecommerce.Domain.Entity;
 
 namespace Company.Ecommerce.Infrastructure.Interface
 {
     public interface ICustomersRepository
     {
+        bool Insert(Customers customer);
+        bool Update(Customers customer);
+        bool Delete(string customerId);
+        Customers DeleteAll(string customerId);
+        IEnumerable<Customers> GetAll();
+
     }
 }
