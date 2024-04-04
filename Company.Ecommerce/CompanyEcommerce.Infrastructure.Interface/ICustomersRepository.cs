4450e535aba165eb4862d1865aa5198f1b41963d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-04-03 23:57:50 -0500

    Final ICustomerRepository

--- Company.Ecommerce/CompanyEcommerce.Infrastructure.Interface/ICustomersRepository.cs
+++ Company.Ecommerce/CompanyEcommerce.Infrastructure.Interface/ICustomersRepository.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Text;
 using Company.Ecommerce.Domain.Entity;
+using System.Threading.Tasks;
 
 namespace Company.Ecommerce.Infrastructure.Interface
 {
     public interface ICustomersRepository
     {
+        #region Methods Synchronous
         bool Insert(Customers customer);
         bool Update(Customers customer);
         bool Delete(string customerId);
-        Customers DeleteAll(string customerId);
+
+        Customers Get(string customerId);
         IEnumerable<Customers> GetAll();
+        #endregion
+
+
+        #region Methods Asynchronous
+        Task<bool> InsertAsync(Customers customer);
+        Task<bool> UpdateAsync(Customers customer);
+        Task<bool> DeleteAsync(string customerId);
+
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+        #endregion
 
     }
 }
