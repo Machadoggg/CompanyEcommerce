fd18ef8b031a836f956543c52fe204411718045c
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-04-04 20:18:45 -0500

    Add NuGet Dapper

--- Company.Ecommerce/Company.Ecommerce.Infrastructure.Repository/CustomersRepository.cs
+++ Company.Ecommerce/Company.Ecommerce.Infrastructure.Repository/CustomersRepository.cs
@@ -2,6 +2,9 @@
 using Company.Ecommerce.Domain.Entity;
 using Company.Ecommerce.Infrastructure.Interface;
 using Company.Ecommerce.Transversal.Common;
+using Dapper;
+using System.Data;
+using System.Threading.Tasks;
 
 namespace Company.Ecommerce.Infrastructure.Repository
 {
