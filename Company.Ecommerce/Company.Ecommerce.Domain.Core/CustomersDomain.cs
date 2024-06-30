5b11f6e80ddd046b1d6f47d137041efcff70e3a0
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 00:55:06 -0500

    Add usings

--- Company.Ecommerce/Company.Ecommerce.Domain.Core/CustomersDomain.cs
+++ Company.Ecommerce/Company.Ecommerce.Domain.Core/CustomersDomain.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Company.Ecommerce.Domain.Entity;
+using Company.Ecommerce.Domain.Interface;
+using Company.Ecommerce.Infrastructure.Interface;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Company.Ecommerce.Domain.Core
 {
