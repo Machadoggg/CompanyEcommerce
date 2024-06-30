f7b19f572ecd880cb13407258217b553c66cfa77
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 00:47:44 -0500

    Add ICustomersDomain methods

--- Company.Ecommerce/Company.Ecommerce.Domain.Interface/ICustomersDomain.cs
+++ Company.Ecommerce/Company.Ecommerce.Domain.Interface/ICustomersDomain.cs
@@ -1,9 +1,28 @@
 ﻿using Company.Ecommerce.Domain.Entity;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Company.Ecommerce.Domain.Interface
 {
     public interface ICustomersDomain
     {
+        #region Methods Synchronous
+        bool Insert(Customers customer);
+        bool Update(Customers customer);
+        bool Delete(string customerId);
+
+        Customers Get(string customerId);
+        IEnumerable<Customers> GetAll();
+        #endregion
+
+
+        #region Methods Asynchronous
+        Task<bool> InsertAsync(Customers customer);
+        Task<bool> UpdateAsync(Customers customer);
+        Task<bool> DeleteAsync(string customerId);
+
+        Task<Customers> GetAsync(string customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+        #endregion
     }
 }
