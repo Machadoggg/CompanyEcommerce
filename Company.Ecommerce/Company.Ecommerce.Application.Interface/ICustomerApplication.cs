48bb1a35a8bb615e90921fd28e572bfca1d49292
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 02:05:54 -0500

    Add usings

--- Company.Ecommerce/Company.Ecommerce.Application.Interface/ICustomerApplication.cs
+++ Company.Ecommerce/Company.Ecommerce.Application.Interface/ICustomerApplication.cs
@@ -1,4 +1,8 @@
-﻿namespace Company.Ecommerce.Application.Interface
+﻿using Company.Ecommerce.Application.DTO;
+using Company.Ecommerce.Transversal.Common;
+using System.Threading.Tasks;
+
+namespace Company.Ecommerce.Application.Interface
 {
     public interface ICustomerApplication
     {
