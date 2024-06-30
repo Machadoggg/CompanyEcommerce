a95a548cb83b7e0c94ced4e7595453adc4238a95
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 02:31:38 -0500

    Add usings

--- Company.Ecommerce/Company.Ecommerce.Application.Main/CustomersApplication.cs
+++ Company.Ecommerce/Company.Ecommerce.Application.Main/CustomersApplication.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using AutoMapper;
+using Company.Ecommerce.Application.DTO;
+using Company.Ecommerce.Application.Interface;
+using Company.Ecommerce.Domain.Entity;
+using Company.Ecommerce.Domain.Interface;
+using Company.Ecommerce.Transversal.Common;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Company.Ecommerce.Application.Main
 {
