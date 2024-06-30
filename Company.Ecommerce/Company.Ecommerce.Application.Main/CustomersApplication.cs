7f838adce5f035affed55b7e6a89f9b11ff39c38
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 02:34:06 -0500

    Dependency inyection

--- Company.Ecommerce/Company.Ecommerce.Application.Main/CustomersApplication.cs
+++ Company.Ecommerce/Company.Ecommerce.Application.Main/CustomersApplication.cs
@@ -10,8 +10,66 @@ using System.Collections.Generic;
 
 namespace Company.Ecommerce.Application.Main
 {
-    public class CustomersApplication 
+    public class CustomersApplication : ICustomerApplication
     {
+        private readonly ICustomersDomain _customersDomain;
+        private readonly IMapper _mapper;
 
+        public CustomersApplication(ICustomersDomain customersDomain, IMapper mapper)
+        {
+            _customersDomain = customersDomain;
+            _mapper = mapper;
+        }
+
+
+        public bool Delete(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<bool>> DeleteAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<CustomersDto> Get(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<IEnumerable<CustomersDto>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<IEnumerable<CustomersDto>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<CustomersDto>> GetAsync(string customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<bool> Insert(CustomersDto customersDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<bool>> InsertAsync(CustomersDto customersDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(CustomersDto customersDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(CustomersDto customersDto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
