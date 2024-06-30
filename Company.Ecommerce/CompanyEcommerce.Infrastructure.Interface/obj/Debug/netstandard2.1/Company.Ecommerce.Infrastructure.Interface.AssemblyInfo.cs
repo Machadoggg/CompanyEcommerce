c6260db077f4cb579fbc200184eb7f8e3ab6d552
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 00:30:47 -0500

    Update changes

--- Company.Ecommerce/CompanyEcommerce.Infrastructure.Interface/obj/Debug/netstandard2.1/Company.Ecommerce.Infrastructure.Interface.AssemblyInfo.cs
+++ Company.Ecommerce/CompanyEcommerce.Infrastructure.Interface/obj/Debug/netstandard2.1/Company.Ecommerce.Infrastructure.Interface.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Ecommerce.Infrastructure.Interface")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f56a74dab2ae2ebf1f2ab8c00ef26e3619b7aeb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Ecommerce.Infrastructure.Interface")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Ecommerce.Infrastructure.Interface")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
