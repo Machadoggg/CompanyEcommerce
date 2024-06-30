28ff71a2b0dafe685645fdab7561c2341445765a
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 00:52:04 -0500

    Add references Company.Ecommerce.Domain.Core project

--- Company.Ecommerce/Company.Ecommerce.Domain.Core/obj/Debug/netstandard2.1/Company.Ecommerce.Domain.Core.AssemblyInfo.cs
+++ Company.Ecommerce/Company.Ecommerce.Domain.Core/obj/Debug/netstandard2.1/Company.Ecommerce.Domain.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Ecommerce.Domain.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b19f572ecd880cb13407258217b553c66cfa77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adff84162f24481fd504a8b45ea15f0981d7707f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Ecommerce.Domain.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Ecommerce.Domain.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
