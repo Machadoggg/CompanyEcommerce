1de14fb491c3cd994a04925ff6da6b8a479060f4
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-30 01:11:12 -0500

    Implement CustomersDomain with methods

--- Company.Ecommerce/Company.Ecommerce.Domain.Core/CustomersDomain.cs
+++ Company.Ecommerce/Company.Ecommerce.Domain.Core/CustomersDomain.cs
@@ -7,8 +7,71 @@ using System.Collections.Generic;
 
 namespace Company.Ecommerce.Domain.Core
 {
-    public class CustomersDomain
+    public class CustomersDomain : ICustomersDomain
     {
+        private readonly ICustomersRepository _customersRepository;
 
+        public CustomersDomain(ICustomersRepository customersRepository)
+        {
+            _customersRepository = customersRepository;
+        }
+
+
+
+        #region Sync methods
+
+        public bool Insert(Customers customer)
+        {
+            return _customersRepository.Insert(customer);
+        }
+
+        public bool Update(Customers customer)
+        {
+            return _customersRepository.Update(customer);
+        }
+
+        public bool Delete(string customerId)
+        {
+            return _customersRepository.Delete(customerId);
+        }
+
+        public Customers Get(string customerId)
+        {
+            return _customersRepository.Get(customerId);
+        }
+
+        public IEnumerable<Customers> GetAll()
+        {
+            return _customersRepository.GetAll();
+        }
+        #endregion
+
+
+        #region Async methods
+        public async Task<bool> InsertAsync(Customers customer)
+        {
+            return await _customersRepository.InsertAsync(customer);
+        }
+
+        public async Task<bool> UpdateAsync(Customers customer)
+        {
+            return await _customersRepository.UpdateAsync(customer);
+        }
+
+        public async Task<bool> DeleteAsync(string customerId)
+        {
+            return await _customersRepository.DeleteAsync(customerId);
+        }
+
+        public async Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            return await _customersRepository.GetAllAsync();
+        }
+
+        public async Task<Customers> GetAsync(string customerId)
+        {
+            return await _customersRepository.GetAsync(customerId);
+        }
+        #endregion
     }
 }
