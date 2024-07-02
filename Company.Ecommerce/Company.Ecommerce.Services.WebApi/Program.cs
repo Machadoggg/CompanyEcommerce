b6601fbdf75297c24cbb63978cb78ac89424ec96
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-02 00:39:24 -0500

    Fix Automapper version between WebApi and Transversal Mapper

--- Company.Ecommerce/Company.Ecommerce.Services.WebApi/Program.cs
+++ Company.Ecommerce/Company.Ecommerce.Services.WebApi/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(x => x.AddProfile(new MappingsProfile()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
