a0f966b9174e6e70c6453f7d6b1e384a74f0570e
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-04 00:30:07 -0500

    JWT - add UsersApplication in App Main

---
+++ Company.Ecommerce/Company.Ecommerce.Application.Main/UsersApplication.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Company.Ecommerce.Application.DTO;
+using Company.Ecommerce.Application.Interface;
+using Company.Ecommerce.Domain.Interface;
+using Company.Ecommerce.Transversal.Common;
+using System;
+
+namespace Company.Ecommerce.Application.Main
+{
+    public class UsersApplication : IUsersApplication
+    {
+        private readonly IUsersDomain _usersDomain;
+        private readonly IMapper _mapper;
+
+        public UsersApplication(IUsersDomain usersDomain, IMapper mapper)
+        {
+            _usersDomain = usersDomain;
+            _mapper = mapper;
+        }
+
+        public Response<UsersDto> Authenticate(string username, string password)
+        {
+            var response = new Response<UsersDto>();
+
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                response.Message = "Parámetros no pueden ser vacios.";
+                return response;
+            }
+
+            try
+            {
+                var user = _usersDomain.Authenticate(username, password);
+                response.Data = _mapper.Map<UsersDto>(user);
+                response.IsSuccess = true;
+                response.Message = "Authenticación exitosa!!!";
+            }
+            catch (InvalidOperationException)
+            {
+                response.IsSuccess = true;
+                response.Message = "Usuario no existe";
+            }
+            catch (Exception e)
+            {
+                response.Message = e.Message;
+            }
+            return response;
+        }
+    }
+}
--- Company.Ecommerce/Company.Ecommerce.Application.Main/obj/Debug/netstandard2.1/Company.Ecommerce.Application.Main.AssemblyInfo.cs
+++ Company.Ecommerce/Company.Ecommerce.Application.Main/obj/Debug/netstandard2.1/Company.Ecommerce.Application.Main.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Ecommerce.Application.Main")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6065af46a0aa34ca631b6a621a0cb936d7ace70f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8e9affa34d452b881574be08dc783515f7c896f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Ecommerce.Application.Main")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Ecommerce.Application.Main")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
