8fd72d51b658a0157d7ff877595aacb75aeadef9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-03 23:53:17 -0500

    JWT - add Users model in entity domain

---
+++ Company.Ecommerce/Company.Ecommerce.Domain.Entity/Users.cs
@@ -0,0 +1,12 @@
+﻿namespace Company.Ecommerce.Domain.Entity
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string UsrName { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Token { get; set; } = default!;;
+    }
+}
--- Company.Ecommerce/Company.Ecommerce.Domain.Entity/obj/Debug/netstandard2.1/Company.Ecommerce.Domain.Entity.AssemblyInfo.cs
+++ Company.Ecommerce/Company.Ecommerce.Domain.Entity/obj/Debug/netstandard2.1/Company.Ecommerce.Domain.Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Ecommerce.Domain.Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6065af46a0aa34ca631b6a621a0cb936d7ace70f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90bbfbf5ccf9332e13ae8a06ee4a94c0133bcf39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Ecommerce.Domain.Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Ecommerce.Domain.Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
