453f54c6f694a0dcb598c8a58702329304cd2866
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-04 00:12:52 -0500

    JWT - add UsersDomain in Domain Core

---
+++ Company.Ecommerce/Company.Ecommerce.Domain.Core/UsersDomain.cs
@@ -0,0 +1,21 @@
+﻿using Company.Ecommerce.Domain.Entity;
+using Company.Ecommerce.Domain.Interface;
+using Company.Ecommerce.Infrastructure.Interface;
+
+namespace Company.Ecommerce.Domain.Core
+{
+    public class UsersDomain : IUsersDomain
+    {
+        private readonly IUsersRepository _usersRepository;
+
+        public UsersDomain(IUsersRepository usersRepository)
+        {
+            _usersRepository = usersRepository;
+        }
+
+        public Users Authenticate(string username, string password)
+        {
+            return _usersRepository.Authhenticate(username, password);
+        }
+    }
+}
