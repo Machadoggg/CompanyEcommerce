1c855cb5a0a04686e5e26ec9dbd2048092bf9448
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-04 01:00:06 -0500

    JWT - mapping class of section Config AppSettings.json

--- Company.Ecommerce/Company.Ecommerce.Services.WebApi/obj/Debug/net7.0/Company.Ecommerce.Services.WebApi.AssemblyInfo.cs
+++ Company.Ecommerce/Company.Ecommerce.Services.WebApi/obj/Debug/net7.0/Company.Ecommerce.Services.WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Ecommerce.Services.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6065af46a0aa34ca631b6a621a0cb936d7ace70f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2540d732effa365661004798457d75cf7df5af1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Ecommerce.Services.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Ecommerce.Services.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
