503ef2620d97114ad0aec41350c5764ea96789b0
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-07 12:59:37 -0500

    Logger - generic class wit 3 methods

--- Company.Ecommerce/Company.Ecommerce.Transversal.Logging/LoggerAdapter.cs
+++ Company.Ecommerce/Company.Ecommerce.Transversal.Logging/LoggerAdapter.cs
@@ -4,8 +4,18 @@ using Microsoft.Extensions.Logging;
 
 namespace Company.Ecommerce.Transversal.Logging
 {
-    public class LoggerAdapter
+    public class LoggerAdapter<T> : IAppLogger<T>
     {
+        public void LogInformation(string message, params object[] args)
+        { 
+        }
 
+        public void LogWarning(string message, params object[] args)
+        {
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+        }
     }
 }
