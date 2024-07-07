2d5cd0768e9d8e34675633913a28d5a33370a2b2
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-07-07 12:42:09 -0500

    Logger - add IAppLogger generic with 3 methods

--- Company.Ecommerce/Company.Ecommerce.Transversal.Common/obj/Debug/netstandard2.1/Company.Ecommerce.Transversal.Common.AssemblyInfo.cs
+++ Company.Ecommerce/Company.Ecommerce.Transversal.Common/obj/Debug/netstandard2.1/Company.Ecommerce.Transversal.Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Company.Ecommerce.Transversal.Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5163a0c90ac9ce79b8fde50b605e68eb8d61605")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481685ba7074af5a86f9d16fa549b77356fba222")]
 [assembly: System.Reflection.AssemblyProductAttribute("Company.Ecommerce.Transversal.Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Company.Ecommerce.Transversal.Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
